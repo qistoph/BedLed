8705459927f03b58dae27ea66cd632cd41649ced
Chris van Marle <qistoph@gmail.com>
2017-06-30 13:36:00 +0200

    Minor updates

--- LightControl.ino
+++ LightControl.ino
@@ -17,33 +17,32 @@ void lightSaveData() {
 }
 
 void lightOn() {
-  //analogWrite(LED_PIN, dimmLevel);
-
-  // LSM   - 1 - Low Speed Mode
+  //TODO: move setup to lightSetup
+  // LSM   - 0 - Low Speed Mode
   // [6:3] - R
   // PCKE  - 0 - Disable PCK (PLL as clock), wait for PLL Lock first
   // PLLE  - 1 - Enable PLL
   // PLOC  - R
-  PLLCSR = _BV(LSM) | _BV(PLLE); // Start PLL, Low Speed Mode
+  PLLCSR = _BV(PLLE); // Start PLL, No Low Speed Mode
   while (!(PLLCSR & (1<<PLOCK))); //Wait for PLL lock
   PLLCSR |= _BV(PCKE); // Use PLL as clock source
 
-  // CTC1       -    1 - Reset timer after compare match with OCR1C
-  // PWM1A      -    1 - Enable PWM mode
-  // COM1A[1:0] -   01 - OC1A cleared on compare match, set on TCNT1 = $00, !OC1A set on compare, cleared on TCNT1 = $00
-  // CS1[3:0]   - 0100 - PCK/8
-  TCCR1 = _BV(CTC1) | _BV(PWM1A) | 0 | _BV(COM1A0) | 0 | _BV(CS12) | 0 | 0;
   OCR1C = dimmMax - 1;
   OCR1A = dimmLevel;
+
+  // CTC1       -    1 - Reset timer after compare match with OCR1C (p.89)
+  // PWM1A      -    1 - Enable PWM mode (p.89)
+  // COM1A[1:0] -   01 - OC1A cleared on compare match, set when TCNT1 = $00. (!OC1A) set on compare match, cleared when TCNT1 = $00. (p.86)
+  // CS1[3:0]   - 0011 - PCK/4 (p.88)
+  TCCR1 = _BV(CTC1) | _BV(PWM1A) | 0 | _BV(COM1A0) | 0 | 0 | _BV(CS11) | _BV(CS10);
 }
 
 void lightOff() {
-  //analogWrite(LED_PIN, 0);
   TCCR1 = 0; // Disable timer 1
 }
 
 void lightDimm() {
-  if(dimmLevel == dimmMax) {
+  if(dimmLevel >= dimmMax) {
     dimmLevel = 1;
   } else {
     dimmLevel = dimmLevel * 2;
